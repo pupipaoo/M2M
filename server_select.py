dbd66d952e17ed69bc88620b6333787ab8998437
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-01 23:34:56 +0800

    111.10.28 M2M-Socket

--- server_select.py
+++ server_select.py
@@ -25,21 +25,21 @@ server_socket.bind(("0.0.0.0", PORT))
 server_socket.listen(10)
 
 # Add server socket to the list of readable connections
-CONNECTION_LIST.append(server_socket)
+CONNECTION_LIST.append(server_socket)  #所有SOCKET形成的LIST
 
 print("Chat server started on port " + str(PORT))
 
-while 1:
+while 1:  #也就是While true
     # Get the list sockets which are ready to be read through select
-    read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[]) #多人連接
+    read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[]) #第一格select得參數代表請求系統監控可以讀取德socket，否則遇到不能讀會卡;第二是可寫;第三是錯誤
     #print('accpet return ')
-    for sock in read_sockets:
+    for sock in read_sockets:  
         
         #New connection
-        if sock == server_socket:
+        if sock == server_socket:   #當sever_socket可以讀取時，代表有cliemt連近來，所以要呼叫accept去抓人下來
             # Handle the case in which there is a new connection recieved through server_socket
             sockfd, addr = server_socket.accept()
-            CONNECTION_LIST.append(sockfd)
+            CONNECTION_LIST.append(sockfd)   #因為有新的可讀取得socket，把cliemt的socket加進去，已讓最上層while輝圈可讀取這格client的訊息近來
             print("Client (%s, %s) connected" % addr)
             
         #Some incoming message from a client
