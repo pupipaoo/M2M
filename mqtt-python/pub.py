9181568dad45aadc2af492b5c0dbb7577acaf438
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-13 06:41:37 +0800

    111.11.3 for mqtt-python

--- mqtt-python/pub.py
+++ mqtt-python/pub.py
@@ -25,10 +25,13 @@ client = mqtt.Client()          #利用MQTT套件產生CLIENT
 client.connect("hq.ittraining.com.tw", 1883, 60)
 
 while True:
-    t0 = random.randint(0,30)
-    t = datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    t0 = random.randint(0,30)       #隨機產生0~30樹自
+    t = datetime.datetime.now().strftime(ISOTIMEFORMAT)     #抓系統現在時間，轉換成時間格式
     payload = {'Temperature' : t0 , 'Time' : t}
     print (json.dumps(payload))
     #要發布的主題和內容
-    client.publish("sensor/temp1", json.dumps(payload))
-    time.sleep(5)
+    client.publish("sensor/temp1", json.dumps(payload))       #在叫做sensor/temp1德topic丟to和t得訊息
+    #訊息被包裝成json格式出去，也就是payload的訊息給他dump成json格式(json跟python的字典很像，只是包裝成字串，因為mqtt只能送字串不能送物件)(json格式有key:value)
+    #key值=semsor和temp1
+    #value值=t0和t
+    time.sleep(5)   #每5秒丟訊息
